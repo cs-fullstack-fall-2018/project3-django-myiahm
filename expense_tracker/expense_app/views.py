59039cccd482ea0d69a14e23a9ac165e4730c993
Christopher Pace <Christopher_pace@ymail.com>
2018-10-29 05:04:23 -0500

    check in

--- expense_tracker/expense_app/views.py
+++ expense_tracker/expense_app/views.py
@@ -10,11 +10,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 
+#if the user is authenticated give them their info that saved in the db
+
+
+def userlist(request):
+    user_list =User.objects.get(name=request.user)
+    context= {'user_list': user_list}
+    return render(request, 'expense_app/index.html',context)
 
 def index(request):
     # user_list = User.objects.filter(name=request.user)
 
-    if (request.user.is_authenticated):
+    if request.user.is_authenticated:
         user_list = User.objects.get(name=request.user)
         print(user_list)
         user_deposits = Deposit.objects.all()
@@ -22,7 +29,7 @@ def index(request):
         user_withdarawls = Withdaraw.objects.all()
         print(user_withdarawls)
         context = {'user_list': user_list, 'user_deposits': user_deposits, 'user_withdarawls': user_withdarawls}
-        return redirect('userindex')
+        return redirect('add')
 
     else:
         return render (request, 'registration/login.html')
@@ -36,18 +43,18 @@ def userIndex(request, pk):
     context = {'user_list': user_list, 'user_deposit': user_deposit, 'user_withdrawal': user_withdrawal}
     return render(request, 'expense_app/index.html', context)
 
-
+#supposed to add user info
 def add(request):
-    if (request.user.is_authenticated.method == 'POST'):
-        form = User(request.POST)
+    if (request.method == 'POST'):
+        form = TheForm(request.POST)
         if (form.is_valid()):
             form.save()
             return redirect("index")
     else:
         form = TheForm()
-    return render(request, 'expense_app/add.html', {'form': form})
+    return render(request, 'expense_app/index.html', {'form': form})
 
-
+#giving them the deposit model and sending them back to the index
 def deposit(request, pk):
     theModel = get_object_or_404(User, pk=pk)
 
@@ -63,7 +70,7 @@ def deposit(request, pk):
         form = DepositForm(instance=theModel)
     return render(request, 'expense_app/deposit.html', {'form': form})
 
-
+#giving them the form model for withdrawal and saving it and sending them back to the index
 def withdraw(request, pk):
     theModel = get_object_or_404(User, pk=pk)
 
@@ -95,22 +102,24 @@ def edit(request, pk):
         form = TheForm(instance=theModel)
     return render(request, 'expense_app/add.html', {'form': form})
 
-
+##sending them to login page
 def log(request):
-    return render(request, 'registration/login.html')
+    if request.method=='POST':
+       return render(request, 'expense_app/add.html')
 
-
+#sending them to logout page
 def log2(request):
     return render(request, 'registration/logged_out.html')
 
 
+ #registering user and suposed to redirect to add fucntion but i cant get it to work
 def register(request):
     if request.method == 'POST':
         f = UserCreationForm(request.POST)
         if f.is_valid():
             f.save()
             messages.success(request, 'Account created successfully')
-            return redirect('add')
+            return redirect("add")
 
     else:
         f = UserCreationForm()
